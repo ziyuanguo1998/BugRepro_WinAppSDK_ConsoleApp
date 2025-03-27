bf4240a41b2ecae1ab65128549df52ffe8faab15
Guo Ziyuan <ziyuanguo@microsoft.com>
2025-03-27 02:26:20 -0700

    init repo

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Windows.AI.Generative;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Thinking!");
+
+            string prompt = GetPrompt(args);
+            string reply = await TestPrompt(prompt);
+
+            Console.WriteLine(reply);
+        }
+
+        private static async Task<string> TestPrompt(string prompt)
+        {
+            if (!LanguageModel.IsAvailable())
+            {
+                var op = await LanguageModel.MakeAvailableAsync();
+            }
+
+            var languageModel = await LanguageModel.CreateAsync();
+            var result = await languageModel.GenerateResponseAsync(prompt);
+
+            return result.Response;
+        }
+
+        private static string GetPrompt(string[] args)
+        {
+            string prompt = string.Empty;
+            if (args.Length > 0)
+            {
+                prompt = args[0];
+            }
+
+            if (string.IsNullOrWhiteSpace(prompt))
+            {
+                prompt = "Provide the molecular formula for glucose.";
+            }
+
+            return prompt;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+﻿###General
+This example is showing an unpackaged + non windows self-contain app to use the most simple Phi Silica API in WinAppSDK 1.7 Experimental3.
+Given my machine is ARM, if have Intel x64 one, simple change can be made but below doesn't specify the details.
+
+##Prerequisites
+1. https://learn.microsoft.com/en-us/windows/ai/apis/phi-silica#prerequisites
+2. Given it is unpackaged app, we need to install the runtime.
+Install the ARM64 version of WinAppSDK 1.7 Experimental 3 from here https://learn.microsoft.com/en-us/windows/apps/windows-app-sdk/downloads#windows-app-sdk-17-experimental
+So far the download link is https://aka.ms/windowsappsdk/1.7/1.7.250127003-experimental3/windowsappruntimeinstall-arm64.exe
+3. This is .net 9 as example, thus ensure you have VS2022 and .net9 installed.
+
+##Steps
+Just open the ConsoleApp.sln by VS2022, and Press Debug to go !
